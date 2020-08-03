abe546e36941012d73fd0836b83f2bdc15ed3ba8
ArhonTs <van14a@ya.ru>
2020-08-03 22:09:23 +0400

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"sync"
 
 	"github.com/dustin/go-humanize"
-	"github.com/opesun/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
 
 type WriteCounter struct {
@@ -38,23 +38,37 @@ func (wc WriteCounter) PrintProgress() {
 	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
 	fmt.Printf("\rDownloading... %s complete", humanize.Bytes(wc.Total))
 }
+func ExecUrl(url string) (*goquery.Document, error){
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	},
+    }
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+			log.Fatalln(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	resp, err := client.Do(req)
+	if err != nil {
+			log.Fatalln(err)
+	}
+	defer resp.Body.Close()	
+return goquery.NewDocumentFromReader(resp.Body)
+}
 
 //получаем url скачивания в зависимости от файлообменника
 func GetUrl(urls string) (rez string, name string) {
-	var re = regexp.MustCompile(`(?m)(https?://.+\.\w+)`)
-
+	var re = regexp.MustCompile(`(?m)(https?://.+\.\w+/)`)
 	for _, match := range re.FindAllString(urls, -1) {
-		switch match {
-		case "https://anonfiles.com":
-			x, _ := goquery.ParseUrl(urls)
-			url := x.Find("#download-url").Attrs("href")
-			rez = url[0]
+		switch match {	
+		case "https://anonfiles.com/","https://anonfile.com/":
+			x,_:= ExecUrl(urls)
+			url,_ := x.Find("#download-url").Attr("href")
+			rez = url
 			name = path.Base(rez)
-		case "https://yadi.sk":
+		case "https://yadi.sk/":
 			x, err := http.Get("https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key=" + urls)
-			if err != nil {
-				log.Fatalln(err)
-			}
 			var result map[string]interface{}
 			json.NewDecoder(x.Body).Decode(&result)
 			rez = fmt.Sprintf("%v", result["href"])
@@ -67,21 +81,42 @@ func GetUrl(urls string) (rez string, name string) {
 		    log.Fatal(err)
 	        }
 			name = unescapedPath
-
-
+		case "https://nofile.org/":
+			x,_:= ExecUrl(urls)
+			url,_ := x.Find("a.btn").Attr("href")
+			rez = "https:"+url
+			fmt.Println(rez)
+			//urlname := x.Find("ul li:first-child").Text()
+			name = "AutoLink_2020_Cs50.zip"
+		case "https://www.upload.ee/": 
+		x,_:= ExecUrl(urls)
+		url,_ := x.Find("#d_l").Attr("href")
+		fmt.Println(url)
+		rez = url
+		name = path.Base(rez)
+		case "https://drive.google.com/":
+			beginstr := strings.LastIndex(urls, "/d/")
+			endstr := strings.LastIndex(urls, "/view")
+			//вырезаем id
+			idstr := urls[beginstr+3:endstr]
+			fmt.Println(idstr)
+			//получаем прямую ссылку загрузки
+			rez = "https://docs.google.com/uc?authuser=1&id="+idstr+"&export=download"
+			fmt.Println(rez)
+			name ="12.rar"
 		default:
 			rez = urls
 			name = path.Base(rez)
 		}
 	}
 	return rez, name
+	
 }
 func main() {
 
 	const LIMIT = 8
 	var throttler = make(chan int, LIMIT)
 	var rez, name string
-
 	file, err := readFile("file.txt")
 	if err != nil {
 		log.Println("Can't read file url.")
@@ -92,12 +127,11 @@ func main() {
 		throttler <- 0
 		wg.Add(1)
 		rez, name = GetUrl(file)
-		fmt.Println(name)
 		go DownloadFile(&wg, rez, name)
 	}
 	//Ожидаем завершение всех потоков
 	wg.Wait()
-	fmt.Println("Загрузка успешно завершена")
+	fmt.Println("Загрузка всех файлов успешно завершена")
 }
 
 ///Читаем файл
@@ -123,11 +157,19 @@ func DownloadFile(wg *sync.WaitGroup, url, names string) error {
 	if err != nil {
 		return err
 	}
-	// Get the data
-	resp, err := http.Get(url)
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	},}
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		out.Close()
-		return err
+			log.Fatalln(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	resp, err := client.Do(req)
+	if err != nil {
+			log.Fatalln(err)
 	}
 	defer resp.Body.Close()
 
@@ -147,6 +189,8 @@ func DownloadFile(wg *sync.WaitGroup, url, names string) error {
 	if err = os.Rename(names+".tmp", names); err != nil {
 		return err
 	}
+
+	fmt.Println("Файл "+names+" успешно загружен")
 	return nil
 
 }
